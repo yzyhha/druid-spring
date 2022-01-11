ae078bec03a36bf5733227e6f357707e69e6cf6a
yzyhha <2502209046@qq.com>
2022-01-11 16:04:28 +0800

    Initial commit

---
+++ src/test/java/com/yzy/data/DataApplicationTests.java
@@ -0,0 +1,31 @@
+package com.yzy.data;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class DataApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+//        获得数据库连接
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+
+        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+        System.out.println("druidDataSource 数据源最大连接数：" + druidDataSource.getMaxActive());
+        System.out.println("druidDataSource 数据源初始化连接数：" + druidDataSource.getInitialSize());
+
+        connection.close();
+    }
+
+}
